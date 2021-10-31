ec43daee86e9b55d90deb037a2a3b7c185b09387
fariha53 <91895990+fariha53@users.noreply.github.com>
2021-10-31 21:13:24 +0600

    Add files via upload

---
+++ Math API/src/oop/Main.java
@@ -0,0 +1,22 @@
+package oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double x = -0.345;
+        int a =4;
+        double y =9.7;
+        double z =7.6;
+
+        System.out.println(Math.sqrt(a));
+        System.out.println(Math.abs(x));
+        System.out.println(Math.ceil(y));
+        System.out.println(Math.floor(y));
+        System.out.println(Math.round(z));
+        System.out.println(Math.max(y,z));
+        System.out.println(Math.min(a,x));
+        System.out.println("Value of PI ="+Math.PI);
+
+    }
+}
